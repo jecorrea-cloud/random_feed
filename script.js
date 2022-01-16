f70dcb1ad4c850cc914ddca2cdb8d91ba3b18437
joaquin <jecorrea@protonmail.com>
2022-01-15 19:41:51 -0500

    added functions to generate the URL

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const container = document.querySelector(".container");
 const unsplashURL = "https://source.unsplash.com/random/";
 const rows = 5;
+
+for (let index = 0; index < rows * 3; index++) {
+  const img = document.createElement("img");
+  img.src = `${unsplashURL}`;
+}
+
+function getRandomSize() {
+  return `${getRandomNr()}x${getRandomNr()}`;
+}
+
+function getRandomNr() {
+  return Math.floor(Math.random() * 10) + 300;
+}
+
+console.log(getRandomNr());
